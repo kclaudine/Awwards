bfdb06f9369244a9adaf04435ea5a2a3417880fd
claudine Kuradusenge <claudinekuradusenge2@gmail.com>
2021-01-21 12:27:33 +0200

    add changes

--- award/settings.py
+++ award/settings.py
@@ -163,9 +163,9 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
